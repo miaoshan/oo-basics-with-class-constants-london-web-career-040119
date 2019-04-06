d9aaea5adcd9ae46d5a5ce396f1c5afadc0c0769
miaoshan <minnieshan@gmail.com>
2019-04-06 12:26:22 +0100

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,8 +2,15 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS= []   #class constant. class constants are shared among all instances.
+                #They all have access to the same data, and if that data should change for some reason,
+                #all instances will know about that change.
+
   def initialize(brand)
     @brand = brand
+    if !BRANDS.include?(brand)
+      BRANDS << brand
+    end
   end
 
   def cobble
